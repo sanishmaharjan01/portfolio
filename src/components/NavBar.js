07be900fb1dad129fab64b0815759632f9679b3e
5ani01 <maharjansanish958@gmail.com>
2025-08-01 16:59:48 +0545

    Updated navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,53 +5,58 @@ import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
-
 export const NavBar = () => {
-    const [activeLink, setActiveLink ]= useState('home');
-    const [scrolled, seScrolled]= useState(false);
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, seScrolled] = useState(false);
 
-    useEffect(() =>{
-        const onScroll =() => {
+    useEffect(() => {
+        const onScroll = () => {
             if (window.scrollY > 50) {
                 seScrolled(true);
-            }else{
+            } else {
                 seScrolled(false);
             }
         }
 
         window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll",onScroll);
+        return () => window.removeEventListener("scroll", onScroll);
     })
 
-const onUpdateActiveLink = (value) =>{
-    setActiveLink(value);
-}
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
-  return (
-    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
-      <Container>
-        <Navbar.Brand href="#home">
-            <img src={logo} alt="logo"/>
-            </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" >
-            <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className ={activeLink ==='home'? 'active navbar-link' : 'navbar-link'} onClick ={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}> Skills</Nav.Link>
-            <Nav.Link href="#project" className ={activeLink ==='project'? 'active navbar-link' : 'navbar-link'} onClick ={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-                <a href="'"><img src ={navIcon1} alt={''}></img></a>
-                <a href="'"><img src ={navIcon2} alt={''}></img></a>
-                <a href="'"><img src ={navIcon3} alt={''}></img></a>
-            </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>let's connect</span></button>
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt="logo" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" >
+                    <span className="navbar-toggler-icon"></span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}> Skills</Nav.Link>
+                        <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="https://www.linkedin.com/in/sanish-maharjan-9b663a294/" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon1} alt="" />
+                            </a>
+                            <a href="https://www.facebook.com/sanishmhzrn958" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon2} alt="" />
+                            </a>
+                            <a href="https://www.instagram.com/theuserispotato_0.o/" target="_blank" rel="noopener noreferrer">
+                                <img src={navIcon3} alt="" />
+                            </a>
+                        </div>
+                        <button className="vvd" onClick={() => console.log('connect')}><span>let's connect</span></button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
