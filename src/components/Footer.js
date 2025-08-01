15ff89ccffbd99877045d4c0e1cbecdc56058564
5ani01 <maharjansanish958@gmail.com>
2025-08-01 16:58:54 +0545

    Updated footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg'; // LinkedIn icon
+import navIcon2 from '../assets/img/nav-icon2.svg'; // Facebook icon
+import navIcon3 from '../assets/img/nav-icon3.svg'; // Instagram icon
 
 export const Footer = () => {
   return (
@@ -14,9 +14,27 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon 1" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon 2" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon 3" /></a>
+              <a
+                href="https://www.linkedin.com/in/sanish-maharjan-9b663a294/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+              <a
+                href="https://www.facebook.com/sanishmhzrn958"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon2} alt="Facebook" />
+              </a>
+              <a
+                href="https://www.instagram.com/theuserispotato_0.o/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={navIcon3} alt="Instagram" />
+              </a>
             </div>
             <p>© {new Date().getFullYear()} Sanish Maharjan. All Rights Reserved.</p>
           </Col>
